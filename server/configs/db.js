34986673de74bf6ce9637326de2cb126900f7889
Manav <manav96544@gmail.com>
2025-11-10 00:12:41 +0530

    feat added

--- server/configs/db.js
+++ server/configs/db.js
@@ -6,6 +6,7 @@ const connectDB = async () =>{
         mongoose.connection.on("connected", ()=> console.log("Database Connected"))
 
         await mongoose.connect(`${process.env.MONGO_URI}/quickblog`);
+        process.exit(1); // stop app if db fails
     }
     catch (error){
         console.log(error.message);
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import connectDB from './configs/db.js';
-import adminRouter from './routes/adminRoutes.js';
-import blogRouter from './routes/blogRoutes.js';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
 
 const app = express();
 
@@ -14,16 +14,17 @@ await connectDB();
 app.use(cors());
 app.use(express.json());
 
-//Routes
-app.get('/',(req,res)=>res.send("API is working"));
-app.use('/api/admin',adminRouter);//for admin page
-app.use('/api/blog', blogRouter);//for blog page
+app.get("/", (req, res) => res.send("API is working"));
 
+// Api Routes
+app.use("/api/admin", adminRouter); //for admin page
+app.use("/api/blog", blogRouter); //for blog page
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>
-console.log(`Server is running on port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running on port http://localhost:${PORT}`)
+);
 
 export default app;
-process.env.PORT
+process.env.PORT;
